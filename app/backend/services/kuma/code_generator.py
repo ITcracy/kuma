1304a73259bfa19ab121d0943ef5621c898a6e97
Navin Karkera <navin@disroot.org>
2020-05-27 12:39:06 +0530

    UPDATE: DOCUMENT CODE
    Signed-off by: Navin Karkera <navin@disroot.org>

--- app/backend/services/kuma/code_generator.py
+++ app/backend/services/kuma/code_generator.py
@@ -13,7 +13,19 @@ class PandasCodeGenerator:
         """
         Converts json request to code
 
-        :param request: Dict
+        Parameters
+        ----------
+        request: dict
+            A request dictionary containing all information related to code generation
+            eg: {"mod": "pd", "func": "read_csv", "args": ["data.csv"], "kwargs": {"index_col": "id"}}
+        save: bool, default False
+            Whether or not to save the result of code execution to a variable
+        display_rows: int, default 10
+            Number of rows to be displayed in html format
+        css_classes: list of str, default None
+            List of classes to be applied to html table
+        variable: str, default "df"
+            Variable name to store the result.
         """
         self.request = request
         self.save = save
@@ -23,6 +35,16 @@ class PandasCodeGenerator:
         self.variable = variable if save else "_current_state"
 
     def validate(self):
+        """
+        Checks for required keys in request dict
+
+        Returns
+        -------
+        None
+        Raises
+        -------
+        ValueError if required keys are not found
+        """
         if not "func" in self.request:
             raise KeyError("'func' required.")
         if not isinstance(self.request.get("args", []), list):
@@ -33,10 +55,24 @@ class PandasCodeGenerator:
             self.request["args"].remove("")
 
     def get_args(self) -> str:
+        """
+        Formats positional arguments from args key in request dict to str
+
+        Returns
+        -------
+        str
+        """
         args = [f'"{x}"' if isinstance(x, str) else f"{x}" for x in self.request.get("args", [])]
         return ", ".join(args)
 
     def get_kwargs(self) -> str:
+        """
+        Formats keyword args from kwargs key in request dict to str
+
+        Returns
+        -------
+        str
+        """
         kwargs = [
             f'{k}="{v}"' if isinstance(v, str) else f"{k}={v}"
             for k, v in self.request.get("kwargs", {}).items()
@@ -44,12 +80,30 @@ class PandasCodeGenerator:
         return ", ".join(kwargs)
 
     def call_args(self) -> str:
+        """
+        Combines args and kwargs as comma separated str
+
+        Returns
+        -------
+        str
+        """
         args = self.get_args()
         kwargs = self.get_kwargs()
         call_args = ", ".join([x for x in (args, kwargs) if x])
         return call_args
 
     def obj_or_module(self) -> str:
+        """
+        Validates mod key from request dict
+
+        Returns
+        -------
+        None
+
+        Raises
+        -------
+        ValueError if mod not in pd or df
+        """
         mod = self.request.get("mod")
         if not mod:
             raise KeyError("'mod' required")
@@ -58,25 +112,46 @@ class PandasCodeGenerator:
         return mod
 
     def user_code(self) -> str:
+        """
+        Generates user specified code from request dict
+
+        Returns
+        -------
+        str
+        """
         mod = self.obj_or_module()
         func = self.request["func"]
         call_args = self.call_args()
         code = f"{self.variable} = {mod}.{func}({call_args})"
         return code
 
-    def meta_code(self, code: str) -> str:
+    def meta_code(self) -> str:
+        """
+        Generates additional code to display result
+
+        Returns
+        -------
+            str
+        """
         pd_html = f"print({self.variable}.head({self.display_rows}).to_html(" \
                   f"classes={self.css_classes}, show_dimensions=True))"
         series_html = f"print({self.variable}.to_frame().head({self.display_rows}).to_html(" \
                       f"classes={self.css_classes}, show_dimensions=True))"
-        code = f"{code}\nif isinstance({self.variable}, pd.DataFrame):"
+        code = f"if isinstance({self.variable}, pd.DataFrame):"
         code = f"{code}\n\t{pd_html}"
         code = f"{code}\nelif isinstance({self.variable}, pd.Series):"
         code = f"{code}\n\t{series_html}"
         return code
 
     def process(self) -> str:
+        """
+        Combines code generated  by user_code and meta_code functions
+
+        Returns
+        -------
+        str
+        """
         self.validate()
         code = self.user_code()
-        code = self.meta_code(code)
-        return code
+        meta_code = self.meta_code()
+        return f"{code}\n{meta_code}"
--- app/backend/services/kuma/inspector.py
+++ app/backend/services/kuma/inspector.py
@@ -7,33 +7,73 @@ from loguru import logger
 
 class Inspector:
     def __init__(self, obj: Any, save: bool = False):
+        """
+        Inspects objects/class and returns related functions etc.
+
+        Parameters
+        ----------
+        obj: Any object or class
+        """
         self.obj = obj
-        self.save = save
+        self._functions = None
 
     @property
     def functions(self) -> Dict[str, Dict]:
-        try:
-            members = inspect.getmembers(self.obj, inspect.isfunction)
-        except NotImplementedError as ni:
-            logger.warning(
-                "Not supported due to below error\n"
-                f"{ni}\n"
-                f"Trying to extract from class of obj i.e. from {self.obj.__class__}"
-            )
-            members = inspect.getmembers(self.obj.__class__, inspect.isfunction)
-        result = {
-            member[0]: Inspector.get_default_args(member[1])
-            for member in members
-            if Inspector.is_public(member[0])
-        }
-        return result
+        """
+        Extracts all functions/methods in a module, class or object
+        along with argument list and default values
+
+        Returns
+        -------
+        Dict, a dictionary with keys as function name and values as dict with
+        keys as argument name and value as default value or type
+        """
+        if not self._functions:
+            try:
+                members = inspect.getmembers(self.obj, inspect.isfunction)
+            except NotImplementedError as ni:
+                logger.warning(
+                    "Not supported due to below error\n"
+                    f"{ni}\n"
+                    f"Trying to extract from class of obj i.e. from {self.obj.__class__}"
+                )
+                members = inspect.getmembers(self.obj.__class__, inspect.isfunction)
+            result = {
+                member[0]: Inspector.get_default_args(member[1])
+                for member in members
+                if Inspector.is_public(member[0])
+            }
+            self._functions = result
+        return self._functions
 
     @staticmethod
-    def is_public(obj: str):
+    def is_public(obj: str) -> bool:
+        """
+        Filter function to get only public functions
+
+        Parameters
+        ----------
+        obj: str, name of function
+
+        Returns
+        -------
+        bool
+        """
         return True if not obj.startswith("_") else False
 
     @staticmethod
     def get_default_args(func: Callable) -> Dict:
+        """
+        Extracts arguments and its default values from a function
+
+        Parameters
+        ----------
+        func: Callable
+
+        Returns
+        -------
+        Dict
+        """
         if not callable(func):
             raise TypeError(f"{func} is not a callable object")
         signature = inspect.signature(func)
