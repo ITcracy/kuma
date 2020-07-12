bf4ab336e92e87300de73e443dcea93e53717d40
Navin Karkera <navin@disroot.org>
2020-07-12 14:41:22 +0530

    REACT AND MATERIAL UI SETUP FOR FRONTEND
    Signed-off by: Navin Karkera <navin@disroot.org>

---
+++ app/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Hello world!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
