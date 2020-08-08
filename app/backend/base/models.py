030d68742351861c4652ae0e20285d98d61ed349
Navin Karkera <navin@disroot.org>
2020-08-08 16:29:30 +0530

    UPDATE: REACT BASIC UI AND ASYNC TASKS SETUP
    Signed-off by: Navin Karkera <navin@disroot.org>

--- app/backend/base/models.py
+++ app/backend/base/models.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Mapping, Optional, Union
+from typing import Dict, List, Mapping, Optional, Union
 
 from loguru import logger
 from sqlalchemy import Table, and_
